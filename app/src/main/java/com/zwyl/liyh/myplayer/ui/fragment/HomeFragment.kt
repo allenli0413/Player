c470acf27d60706726997d4f700919ee530ad934
liyh <liyh@zhiwyl.com>
2018-04-23 14:03:09 +0800

    删除idea文件

--- app/src/main/java/com/zwyl/liyh/myplayer/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/zwyl/liyh/myplayer/ui/fragment/HomeFragment.kt
@@ -5,7 +5,11 @@ import android.view.View
 import com.zwyl.liyh.myplayer.R
 import com.zwyl.liyh.myplayer.adapter.HomeAdapter
 import com.zwyl.liyh.myplayer.base.BaseFragment
+import com.zwyl.liyh.myplayer.util.URLProviderUtils
 import kotlinx.android.synthetic.main.fragment_home.*
+import okhttp3.*
+import org.jetbrains.anko.info
+import java.io.IOException
 
 /**
  * Author: liyh
@@ -19,5 +23,31 @@ class HomeFragment : BaseFragment() {
     override fun initListener() {
         recl_fragment_home.layoutManager = LinearLayoutManager(activity)
         recl_fragment_home.adapter = HomeAdapter()
+    }
+
+    override fun initData() {
+        //数据初始化
+        loadDatas()
+    }
+
+    private fun loadDatas() {
+        val path = URLProviderUtils.getHomeUrl(0, 20)
+        val client = OkHttpClient()
+        val request = Request.Builder()
+                .get()
+                .url(path)
+                .build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call?, e: IOException?) {
+                info { "网络请求失败${Thread.currentThread().name}" }
+                myToast("网络请求失败")
+            }
+
+            override fun onResponse(call: Call?, response: Response?) {
+                info { "网络请求成功${Thread.currentThread().name}" }
+                myToast("网络请求成功")
+            }
+
+        })
     }
 }
