a3cc33771a7397aab3001ce915406259dca6c15e
liyanhui <yanhui.li@iconbow.com>
2018-05-17 20:52:47 +0800

    音乐播放控制器

--- app/src/main/java/com/zwyl/liyh/myplayer/service/AudioService.kt
+++ app/src/main/java/com/zwyl/liyh/myplayer/service/AudioService.kt
@@ -1,14 +1,17 @@
 package com.zwyl.liyh.myplayer.service
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Binder
 import android.os.IBinder
+import com.zwyl.liyh.myplayer.R.id.mode
 import com.zwyl.liyh.myplayer.model.VBangItemBean
 import org.greenrobot.eventbus.EventBus
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import java.util.*
 
 /**
  * @author  Yahri Lee
@@ -21,8 +24,21 @@ class AudioService : Service(), AnkoLogger {
     var list: ArrayList<VBangItemBean>? = null
     var position: Int = 0
     val audioBinder by lazy { AudioBinder() }
+    val sp by lazy { getSharedPreferences("config", Context.MODE_PRIVATE) }
+
+    companion object {
+        val MODE_ALL_NO_LOOP = 0    //顺序播放
+        val MODE_ALL_LOOP = 1       //全部循环
+        val MODE_SINGLE = 2         //单曲播放
+        val MODE_SINGLE_LOOP = 3    //单曲循环播放
+        val MODE_RANDOM = 4         //随机播放
+    }
+
+    var mode = MODE_ALL_NO_LOOP
+
     override fun onCreate() {
         info { "onCreate() is called" }
+        mode = sp.getInt("mode", MODE_ALL_LOOP)
         super.onCreate()
     }
 
@@ -54,7 +70,62 @@ class AudioService : Service(), AnkoLogger {
         super.onDestroy()
     }
 
-    inner class AudioBinder : Binder(), IService, MediaPlayer.OnPreparedListener {
+    inner class AudioBinder : Binder(), IService, MediaPlayer.OnPreparedListener, MediaPlayer.OnCompletionListener {
+        //获取当前播放模式
+        override fun getCurMode(): Int = mode
+
+        //设置播放模式
+        override fun setPlayMode(currentMode: Int) {
+            mode = currentMode
+            sp.edit().putInt("mode", mode).commit()
+        }
+
+
+        //播放结束监听
+        override fun onCompletion(mp: MediaPlayer?) {
+            playNextByMode()
+        }
+
+        //根据模式播放下一曲
+        private fun playNextByMode() {
+            when (mode) {
+                MODE_ALL_NO_LOOP -> {
+                    list?.let {
+                        position = (position + 1) % it.size
+                    }
+                }
+                MODE_ALL_NO_LOOP -> {
+                    list?.let {
+                        if (position == it.size) return
+                        else position++
+                    }
+                }
+                MODE_SINGLE -> {
+                    return
+                }
+                MODE_RANDOM -> {
+                    list?.let {
+                        val tempPos = position
+                        while (tempPos == position) {
+                            position = Random().nextInt(it.size)
+                        }
+                    }
+                }
+            }
+            playItem()
+        }
+
+        //设置播放进度
+        override fun setProgress(progress: Int) {
+            mediaPlayer?.seekTo(progress)
+        }
+
+        //获取总进度
+        override fun getDuration(): Int? = mediaPlayer?.duration
+
+        //获取当前进度
+        override fun getProgress(): Int? = mediaPlayer?.currentPosition
+
         /**
          * 通知界面更新
          */
@@ -63,7 +134,9 @@ class AudioService : Service(), AnkoLogger {
         }
 
         override fun isPlaying(): Boolean? = mediaPlayer?.isPlaying
-
+        /**
+         * 更新播放状态
+         */
         override fun updatePlayStatus(): Boolean? {
             val isPlaying = isPlaying()
             isPlaying?.let {
@@ -77,20 +150,27 @@ class AudioService : Service(), AnkoLogger {
             notfityUpdateUi()
         }
 
-        override fun playItem() {
+        fun playItem() {
             mediaPlayer = mediaPlayer ?: MediaPlayer()
             mediaPlayer?.let {
                 it.setOnPreparedListener(this)
+                it.setOnCompletionListener(this)
                 it.setDataSource(list?.get(position)?.data)
                 it.prepareAsync()
             }
         }
 
-        override fun start() { mediaPlayer?.start() }
+        override fun start() {
+            mediaPlayer?.start()
+        }
 
-        override fun pause() { mediaPlayer?.pause()}
+        override fun pause() {
+            mediaPlayer?.pause()
+        }
 
-        override fun stop() { mediaPlayer?.stop()}
+        override fun stop() {
+            mediaPlayer?.stop()
+        }
 
         override fun prev() {
         }
